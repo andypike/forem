327a5c0cf4741a3e30972b0ef74d4b391a83b5c1
Andy Pike <andy@andypike.com>
2013-08-04 19:09:31 +0100

    Order admin categories list by ordinal

--- app/controllers/forem/categories_controller.rb
+++ app/controllers/forem/categories_controller.rb
@@ -2,5 +2,9 @@ module Forem
   class CategoriesController < Forem::ApplicationController
     helper 'forem/forums'
     load_and_authorize_resource
+
+    def index 
+      @categories = Forem::Category.all.order("ordinal ASC")
+    end
   end
 end
